3ccb01244e8e3be62a37400f2019edb021730b53
novaventurelane <linklemodding@gmail.com>
2022-04-24 00:10:43 -0700

    Update RenderLayers.java

--- src/main/java/net/linkle/cozy/client/RenderLayers.java
+++ src/main/java/net/linkle/cozy/client/RenderLayers.java
@@ -6,6 +6,8 @@ import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.block.Block;
 import net.minecraft.client.render.RenderLayer;
 
+import static net.linkle.cozy.init.C_Blocks.*;
+
 @Environment(EnvType.CLIENT)
 public class RenderLayers {
     
@@ -18,6 +20,15 @@ public class RenderLayers {
         
         // Example
         //put(Blocks.GLASS, cullout);
+        put(GLOW_BERRY_LANTERN, translucent);
+        put(FLOWER_LANTERN, translucent);
+        put(AMETHYST_LANTERN, translucent);
+        put(SHORT_LILAC_BUSH, translucent);
+        put(SHORT_ROSE_BUSH, translucent);
+        put(SHORT_PEONY_BUSH, translucent);
+        put(AZALEA_ACACIA_DOOR, translucent);
+        put(AZALEA_JUNGLE_DOOR, translucent);
+        put(AZALEA_OAK_DOOR, translucent);
     }
     
     private static void put(Block block, RenderLayer layer) {
